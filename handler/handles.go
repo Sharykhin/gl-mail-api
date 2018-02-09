e34968415458638b9b0b2d34ce7d864cef739dd0
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-02-09 10:27:53 +0300

    started working on login method

--- handler/handles.go
+++ handler/handles.go
@@ -6,3 +6,8 @@ func pong(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("pong"))
 }
+
+func login(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
--- handler/routing.go
+++ handler/routing.go
@@ -8,6 +8,7 @@ import (
 func Handler() http.Handler {
 	r := mux.NewRouter()
 	//r.Handle("/login", http.HandlerFunc(HandleLogin))
-	r.Handle("/ping", http.HandlerFunc(pong))
+	r.Handle("/ping", http.HandlerFunc(pong)).Methods("GET")
+	r.Handle("/login", http.HandlerFunc(login)).Methods("POST")
 	return r
 }
