8c400876c09d90f11ff014471970118e0deeafa7
Sharykhin <siarhei.sharykhin@gmail.com>
2018-02-19 00:17:01 +0300

    some work in entity

--- entity/entity.go
+++ entity/entity.go
@@ -29,7 +29,7 @@ func (fmr FailMailRequest) Validate() error {
 	if strings.Trim(fmr.Action, " ") == "" {
 		return fmt.Errorf("action is required")
 	}
-
+	fmt.Println("HAHA", fmr.Payload)
 	if fmr.Payload == nil {
 		return fmt.Errorf("payload is required")
 	}
@@ -52,8 +52,27 @@ func (t JSONTime) MarshalJSON() ([]byte, error) {
 // Payload is a specific time for json struct of a payload
 type Payload map[string]json.RawMessage
 
+//@QUESTION: is it a good way to convert data from storage to a specific struct value
+
 // Scan implements a common interface for scanning values from database source to a specific struct
 func (p *Payload) Scan(v interface{}) error {
 	err := json.Unmarshal(v.([]byte), &p)
 	return err
+}
+
+// Test implementation
+func (p Payload) UnmarshalJSON(b []byte) error {
+	var stuff map[string]json.RawMessage
+
+	err := json.Unmarshal(b, &stuff)
+	if err != nil {
+		return fmt.Errorf("payload must be a valid json")
+	}
+	if p == nil {
+		p = map[string]json.RawMessage{}
+	}
+	for key, value := range stuff {
+		p[key] = value
+	}
+	return nil
 }
