b9b0ab0386f03c1b4263c276f179c01da43c041b
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-02-19 10:55:21 +0300

    cover test

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/Sharykhin/gl-mail-api/entity"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -21,6 +22,10 @@ func (m mockStorage) GetList(ctx context.Context, limit, offset int) ([]entity.F
 
 func (m mockStorage) Count(ctx context.Context) (int, error) {
 	return 0, nil
+}
+
+func (m mockStorage) Create(ctx context.Context, mr entity.FailMailRequest) (*entity.FailMail, error) {
+	return nil, nil
 }
 
 type successMockStorage struct {
@@ -39,6 +44,32 @@ func (m successMockStorage) Create(ctx context.Context, mr entity.FailMailReques
 
 type errorMockStorage struct {
 	mockStorage
+}
+
+type successMockList struct {
+	mockStorage
+}
+
+func (m successMockList) GetList(ctx context.Context, limit, offset int) ([]entity.FailMail, error) {
+	ret := m.Called(ctx, limit, offset)
+	return ret.Get(0).([]entity.FailMail), nil
+}
+
+func (m successMockList) Count(ctx context.Context) (int, error) {
+	return 10, nil
+}
+
+type errorMockList struct {
+	mockStorage
+}
+
+func (m errorMockList) GetList(ctx context.Context, limit, offset int) ([]entity.FailMail, error) {
+	m.Called(ctx, limit, offset)
+	return nil, errors.New("something went wrong")
+}
+
+func (m errorMockList) Count(ctx context.Context) (int, error) {
+	return 0, nil
 }
 
 func (m *errorMockStorage) Create(ctx context.Context, mr entity.FailMailRequest) (*entity.FailMail, error) {
@@ -95,5 +126,72 @@ func TestCreate(t *testing.T) {
 		if expectedErr.Error() != err.Error() {
 			t.Errorf("expected error %s, but got another error %s", expectedErr, err)
 		}
+	})
+}
+
+func TestGetList(t *testing.T) {
+	t.Run("success getting", func(t *testing.T) {
+		ctx := context.Background()
+		cc, _ := context.WithCancel(ctx)
+
+		var fm = []entity.FailMail{
+			{
+				ID:      19,
+				Action:  "register",
+				Payload: []byte(`{}`),
+				Reason:  "test reason",
+			},
+			{
+				ID:      20,
+				Action:  "register",
+				Payload: []byte(`{}`),
+				Reason:  "test reason",
+			},
+		}
+
+		mockS := new(successMockList)
+		mockS.On("GetList", cc, 10, 0).Return(fm, nil).Once()
+		mockS.On("Count", cc).Return(10, nil).Once()
+
+		fm, c, err := GetList(ctx, mockS, 10, 0)
+		if err != nil {
+			t.Errorf("unexpected an errror: %v", err)
+		}
+
+		assert.Equal(t, 10, c, "count should be equal 10")
+		assert.NotNil(t, fm, "an array of failed mails can not be nil")
+		assert.Equal(t, 2, len(fm), "number of mails should be equal 2")
+	})
+
+	t.Run("bad getting", func(t *testing.T) {
+		ctx := context.Background()
+		cc, _ := context.WithCancel(ctx)
+
+		var fm = []entity.FailMail{
+			{
+				ID:      19,
+				Action:  "register",
+				Payload: []byte(`{}`),
+				Reason:  "test reason",
+			},
+			{
+				ID:      20,
+				Action:  "register",
+				Payload: []byte(`{}`),
+				Reason:  "test reason",
+			},
+		}
+
+		mockS := new(errorMockList)
+		mockS.On("GetList", cc, 10, 0).Return(fm, nil).Once()
+		mockS.On("Count", cc).Return(10, nil).Once()
+
+		fm, c, err := GetList(ctx, mockS, 10, 0)
+		if err == nil {
+			t.Errorf("expected error but got retusl: %v", fm)
+		}
+
+		assert.Nil(t, fm, "result  should be nil")
+		assert.Equal(t, 0, c, "count should return zero")
 	})
 }
